896f002cb308f7fe5db16558b5ef4c52717eaafd
aselwilliams <aselwilliams22@gmail.com>
2022-09-12 22:07:11 -0500

    modified

---
+++ src/app/services/firebaseService/firebaseServiceConfig.js
@@ -0,0 +1,25 @@
+const prodConfig = {
+  apiKey: "AIzaSyAiW0juurABzfesp39FfCBGzkX1ogPrGhM",
+  authDomain: "vlmd-77b18.firebaseapp.com",
+  databaseURL: "https://vlmd-77b18-default-rtdb.firebaseio.com",
+  projectId: "vlmd-77b18",
+  storageBucket: "vlmd-77b18.appspot.com",
+  messagingSenderId: "1066128254966",
+  appId: "1:1066128254966:web:c1ebc4dd9b69b1e36709d7",
+  measurementId: "G-R87MQW7C6M"
+};
+
+const devConfig = {
+  apiKey: "AIzaSyAiW0juurABzfesp39FfCBGzkX1ogPrGhM",
+  authDomain: "vlmd-77b18.firebaseapp.com",
+  databaseURL: "https://vlmd-77b18-default-rtdb.firebaseio.com",
+  projectId: "vlmd-77b18",
+  storageBucket: "vlmd-77b18.appspot.com",
+  messagingSenderId: "1066128254966",
+  appId: "1:1066128254966:web:c1ebc4dd9b69b1e36709d7",
+  measurementId: "G-R87MQW7C6M"
+};
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+export default config;
